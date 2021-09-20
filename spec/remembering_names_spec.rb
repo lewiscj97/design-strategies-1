f7037f026a82e8f5c2e7deec82d3607e6e7f8b67
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-20 12:08:27 +0100

    initial commit

---
+++ spec/remembering_names_spec.rb
@@ -0,0 +1,11 @@
+describe "the add_person method" do
+  it "tells us that the person was added" do
+    expect(add_person("Lewis")).to eq "Lewis was recorded"
+  end
+end
+
+describe "the show_people method" do
+  it "returns an array of all people added" do
+    expect(show_people).to eq ["Lewis", "Ana"]
+  end
+end
