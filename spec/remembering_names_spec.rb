6b4a416481fc4bb4362161cbe5f2aa51b58ce298
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-20 12:23:55 +0100

    test: update tests for deeper functionality and OOP

--- spec/remembering_names_spec.rb
+++ spec/remembering_names_spec.rb
@@ -2,12 +2,23 @@ require 'remembering_names'
 
 describe "the add_person method" do
   it "tells us that the person was added" do
-    expect(add_person("Lewis")).to eq "Lewis was recorded"
+    names = RememberingNames.new()
+    names.add_person("Lewis")
+    expect(names.add_person("Lewis")).to eq "Lewis was recorded"
+  end
+
+  it "adds the person to an array" do
+    names = RememberingNames.new()
+    names.add_person("Lewis")
+    expect(names.show_people).to eq ["Lewis"]
   end
 end
 
 describe "the show_people method" do
   it "returns an array of all people added" do
-    expect(show_people).to eq ["Lewis", "Ana"]
+    names = RememberingNames.new()
+    names.add_person("Lewis")
+    names.add_person("Ana")
+    expect(names.show_people).to eq ["Lewis", "Ana"]
   end
 end
